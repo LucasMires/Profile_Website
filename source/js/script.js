6ed8c4c85e29debaeb668bb3b5651890c06f222a
Lucas <lucasmires10@gmail.com>
2022-08-01 12:40:31 -0300

    Primeira atualizaçao de muitas, hoje adicionei as funcionalidades de position, e pseudo classes como Before and After.

---
+++ source/js/script.js
@@ -0,0 +1,49 @@
+//---- Cabeçalho -----\\
+let header = document.createElement("header")
+let logo = document.createElement("h1")
+let cabecalhoPesquisa = document.createElement("input")
+let contatos = document.createElement("a")
+
+header.classList.add("cabecalho")
+logo.classList.add("logoCabecalho")
+cabecalhoPesquisa.classList.add("cabecalhoPesquisa")
+contatos.classList.add("cabecalhoContatos")
+
+logo.innerText = "Logo_Página"
+
+cabecalhoPesquisa.placeholder = "Pesquisar Modelo"
+
+contatos.innerText = "Contatos"
+contatos.href = "#"
+
+header.append(logo, cabecalhoPesquisa, contatos)
+document.body.appendChild(header)
+//---- Cabeçalho Finalizado ----\\
+
+//---- Profile Card ----\\
+let section = document.createElement("section")
+let figure = document.createElement("figure")
+let profileModel = document.createElement("img")
+let divisoria = document.createElement("div")
+let description = document.createElement("p")
+
+
+section.classList.add("sessaoCard")
+figure.classList.add("imgCaixa")
+profileModel.classList.add("imgCard")
+divisoria.classList.add("caixaDescription")
+description.classList.add("description")
+
+profileModel.src = "./img/Pic Model.jpeg"
+profileModel.alt = "Model_Photo"
+
+description.innerText = "O modelo selecionado é Lucas Mires, estudante da Kenzie Academy Brasil. Este projeto de perfil foi inspirado nas aulas de CSS do Canvas. Para qualquer um que tenha alguma dificuldade com CSS possa ultilizar esta página como um exemplo/possibilidade de Cards no CSS." 
+
+
+section.append(figure, divisoria)
+divisoria.append(description)
+figure.appendChild(profileModel)
+
+document.body.appendChild(section)
+
+//---- Profile Card Finalizado----\\
